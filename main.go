d086895a63c3cbef426e6836620a9c8142abf007
Jonathan Diosa <jadiosa@gmail.com>
2016-01-30 13:11:15 -0500

    port configuration added

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/jadiosa/fishy-river-api/Godeps/_workspace/src/github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run(":8080") // listen and server on 0.0.0.0:8080
+	router.Run(":" + port) // listen and server on 0.0.0.0:8080
 }
